189f926750a6b11418f048a2d12acbdc4ce05f9c
kaciafr <162875953+kaciafr@users.noreply.github.com>
2025-12-23 15:12:59 +0100

    Test

--- git-sync.ps1
+++ git-sync.ps1
@@ -1,4 +1,13 @@
-git add .
-git commit -m "auto"
-git pull
-git push origin main
+Add-Type -AssemblyName Microsoft.VisualBasic
+$message = [Microsoft.VisualBasic.Interaction]::InputBox('Entrez le message du commit', 'Git Commit', 'auto')
+
+if ($message) {
+    git add .
+    git commit -m $message
+    git pull
+    git push origin main
+    
+    [System.Windows.Forms.MessageBox]::Show("Commit effectué avec succès!", "Git")
+} else {
+    [System.Windows.Forms.MessageBox]::Show("Commit annulé", "Git")
+}
